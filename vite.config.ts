90886a55435d012e0eb5847dbaf19d13c5105a60
lucas.esperidiao <lucas.esperidiao@olist.com>
2021-08-22 14:31:51 -0300

    Add vue i18n

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
+  publicDir: './public',
   plugins: [vue()],
   css: {
     preprocessorOptions: {
