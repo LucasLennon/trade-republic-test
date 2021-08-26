1038496b70963765b1a637d76560a20402751b74
lucas.esperidiao <lucas.esperidiao@olist.com>
2021-08-26 19:58:14 -0300

    Remove unecessary class objects

--- src/components/Box/index.vue
+++ src/components/Box/index.vue
@@ -9,41 +9,6 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'Box',
-  props: {
-    xs: {
-      type: String,
-      required: false,
-    },
-    sm: {
-      type: String,
-      required: false,
-    },
-    md: {
-      type: String,
-      required: false,
-    },
-    lg: {
-      type: String,
-      required: false,
-    },
-    xl: {
-      type: String,
-      required: false,
-    },
-  },
-  setup(props) {
-    const classObject = {
-      [`xs-w-${props.xs}`]: !!props.xs,
-      [`sm-w-${props.sm}`]: !!props.sm,
-      [`md-w-${props.md}`]: !!props.md,
-      [`lg-w-${props.lg}`]: !!props.lg,
-      [`xl-w-${props.xl}`]: !!props.xl,
-    };
-
-    return {
-      classObject,
-    };
-  },
 });
 </script>
 
--- src/components/Flex/index.vue
+++ src/components/Flex/index.vue
@@ -6,11 +6,9 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import Box from '@/components/Box/index.vue';
 
 export default defineComponent({
   name: 'Flex',
-  extends: Box,
   props: {
     alignItems: {
       type: String,
